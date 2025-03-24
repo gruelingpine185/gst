6176b2bb30a9e3627d12e42112487bda1ca9df89
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2025-03-24 17:28:17 -0400

    added error pages, organized code

---
+++ src/loaders/express.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const config = require('../config');
+const express = require('express');
+const { rateLimit } = require('express-rate-limit');
+
+const company = {
+    name: "GST",
+    fullName: "Global Security Technologies"
+};
+
+const handleError = ({app, error}) => {
+    app.use((req, res, next) => {
+        res.status(error.code).render('pages/error', {
+            company: company,
+            error: error
+        });
+    });
+};
+
+const setRoutes = ({app}) => {
+    app.get('/', (req, res) => {
+        res.render('pages/index', {
+            company: company
+        });
+    });
+
+    handleError({
+        app: app,
+        error: {
+            code: 404,
+            message: "Looks like you\'re lost :/"
+        }
+    });
+
+    handleError({
+        app: app,
+        error: {
+            code: 529,
+            message: "Too many requests, try again later."
+        }
+    });
+
+    app.listen(config.port, () => {
+        console.log(`Live at http://localhost:${config.port}`);
+    });
+};
+
+const init = ({app}) => {
+    const limiter = rateLimit({
+        windowMs: 1000 * 60 * 15,
+        limit: 100,
+        standardHeaders: true,
+        legacyHeaders: false
+    });
+
+    app.use(express.static(path.join(__dirname, '..', 'public')));
+    app.use(limiter);
+    app.set('views', path.join(__dirname, '..', 'views'));
+    app.set('view engine', 'ejs');
+    setRoutes({app});
+};
+
+module.exports = {
+    init
+}
